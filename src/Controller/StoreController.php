df226bd81b36be5918ea85eac2c56662f9f29b93
MohamedAmine.Oueslati <wesamin1991@gmail.com>
2020-12-08 20:59:13 +0100

    working on register+login

--- src/Controller/StoreController.php
+++ src/Controller/StoreController.php
@@ -3,19 +3,60 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+
 use App\Entity\Books;
+use App\Entity\Users;
 
 class StoreController extends AbstractController
 {
+
+    /**
+     * @Route("/Register", name="register")
+     */
+    public function register(Request $request, ObjectManager $manager): Response
+    {
+
+        $user = new Users();
+        $form = $this->createFormBuilder($user)
+                     ->add('username',TextType::class,["attr" => ["placeholder" => "Username"]])
+                     ->add('email',EmailType::class,["attr" => ["placeholder" => "Email"]])
+                     ->add('password',PasswordType::class,["attr" => ["placeholder" => "Password"]])
+                     ->add('save', SubmitType::class, ['label' => 'Register'])
+                     ->getForm();
+
+
+                     $manager->persist($user);
+                     $manager->flush();
+                     
+        return $this->render('store/register.html.twig', [
+            'formUser' => $form->createView(),
+        ]);
+
+    }
+
     /**
      * @Route("/Login", name="login")
      */
-    public function login(): Response
+    public function login(Request $request): Response
     {
+        $user = new Users();
+        $form = $this->createFormBuilder($user)
+                     ->add('email',EmailType::class,["attr" => ["placeholder" => "Email"]])
+                     ->add('password',PasswordType::class,["attr" => ["placeholder" => "Password"]])
+                     ->add('save', SubmitType::class, ['label' => 'Login'])
+                     ->getForm();
+
+
         return $this->render('store/login.html.twig', [
-            'controller_name' => 'StoreController',
+            'formUser' => $form->createView(),
         ]);
     }
 
